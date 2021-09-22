18d2d71fee5dfddbca14dcff9d941b6bbaa1c9bd
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2021-09-22 09:44:24 +0200

    Add google analytics tracking

--- src/index.js
+++ src/index.js
@@ -3,44 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
-import About from "./components/about/about";
-import Footer from "./components/navs/footer";
-import Header from "./components/navs/header";
-import HomePage from "./components/home-page/home-page";
-import Notfound from "./components/notfound";
-import RelatedProjects from "./components/about/related-projects";
-import PrivacyNotice from "./components/privacy-notice/privacy-notice";
-import CookieConsent from "react-cookie-consent";
+import App from './App';
 
-const routing = (
-    <Router basename={process.env.REACT_APP_ROUTER_BASE || ''}>
-        <Header />
-        <div id="moultdb-body">
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route path="/about/moultdb" component={About} />
-                <Route path="/about/related-projects" component={RelatedProjects} />
-                <Route path="/about/privacy-notice" component={PrivacyNotice} />
-                <Route component={Notfound} />
-            </Switch>
-            <CookieConsent
-                location="bottom"
-                buttonText="Do not show this banner again"
-                cookieName="__Host-moultdb-privacy-policy"
-                cookieValue="1"
-                style={{ backgroundColor: "rgba(0, 0, 0, 0.8)" }}
-                buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
-                expires={365}
-                sameSite={"strict"}
-                cookieSecurity={"true"}
-            >
-                This website requires cookies, and limited processing of your personal data in order to function.
-                By using the site you are agreeing to this as outlined in our <Link to="/about/privacy-notice">privacy notice</Link>.
-            </CookieConsent>
-        </div>
-        <Footer />
-    </Router>
+ReactDOM.render(
+    <App />,
+    document.getElementById('moultdb-content')
 );
-
-ReactDOM.render(routing, document.getElementById('moultdb-content'));
