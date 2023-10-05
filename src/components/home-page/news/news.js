506f7c778ede9a7854f19a1c8b29fa78b13f91b4
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2023-10-05 10:55:08 +0200

    Remove twitter timeline

--- src/components/home-page/news/news.js
+++ src/components/home-page/news/news.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import './news.css';
-import { TwitterTimelineEmbed } from 'react-twitter-embed';
-import AdBlockDetect from '../../ad-block-detect/detect';
 
 const News = () => {
     return (
         <section className="moultdb-news">
             <h2>Last news</h2>
-            <AdBlockDetect />
-            <div className="twitter-embed col-xl-10 col-xxl-9">
-                <TwitterTimelineEmbed sourceType="profile" screenName="moultdb" noHeader placeholder="Loading..."
-                                      options={{tweetLimit: "5", lang: "en", dnt: "true", cards: "hidden"}}
-                />
-            </div>
+            <p>Click <a target="_blank" rel={"noopener noreferrer"}
+                        href="https://twitter.com/moultdb">here</a> to see news on X.</p>
         </section>
     );
 };
