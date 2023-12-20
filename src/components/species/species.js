10bad08b433b2dfa54d3d6b0a432ec5ed188754f
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2023-12-20 16:46:19 +0100

    Improve species display

--- src/components/species/species.js
+++ src/components/species/species.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import {Link} from "react-router-dom";
 import {useParams} from "react-router-dom";
 import './species.css';
 import {getMainUrl} from "../../common/taxon-utils";
@@ -7,24 +8,81 @@ import PhenotypicData from "../data/phenotypic-data";
 import MoultdbService from "../../services/moultdb.service";
 import GenomeData from "../data/genome-data";
 
+function displayXref(taxon) {
+    const groupedByDataSource = taxon.dbXrefs
+        .filter(element => element.accession !== null)
+        .sort((a, b) => {
+            const comparisonByDataSource = b.dataSource.name.localeCompare(a.dataSource.name);
+            const comparisonByName = a.name.localeCompare(b.name);
+            const comparisonByMain = b.main - a.main;
+            const comparisonByAccession = a.accession - b.accession;
+            
+            if (comparisonByDataSource !== 0) return comparisonByDataSource;
+            if (comparisonByMain !== 0) return comparisonByMain;
+            if (comparisonByName !== 0) return comparisonByName;
+            return comparisonByAccession;
+        })
+        .reduce((acc, element) => {
+            const key = element.dataSource.name;
+            if (!acc[key]) {
+                acc[key] = [];
+            }
+            acc[key].push(element);
+            return acc;
+        }, {});
 
-function getXrefUl(taxon) {
-    return <ul className="xref">
-        {taxon.dbXrefs
-            .filter(element => element.accession !== null)
-            .sort((a, b) => {
-                // Compare by data source names (string)
-                const comparisonByName = a.dataSource.name.localeCompare(b.dataSource.name);
-                // If names are equals, compare by accession (integer)
-                return comparisonByName !== 0 ? comparisonByName : a.accession - b.accession;
-            })
-            .map((element, index) => (
-                <li key={index}>
-                    {element.dataSource.name}: <a href={element.xrefURL} rel="noopener noreferrer" target="_blank">
-                    {element.accession}</a>
-                </li>
-            ))}
-    </ul>;
+    return (
+        <div className="key-value-pair">
+            <span className="key">Cross-reference(s)</span>
+            <span className="value">
+                <ul>
+                    {Object.keys(groupedByDataSource).map(dataSourceName => (
+                        <li key={dataSourceName}>
+                            <strong>{dataSourceName}</strong>
+                            <ul className="xref">
+                                {groupedByDataSource[dataSourceName].map((element, index) => (
+                                    <li key={index}>
+                                        <a href={element.xrefURL} rel="noopener noreferrer" target="_blank">
+                                            {element.name}
+                                        </a>
+                                        {index < groupedByDataSource[dataSourceName].length - 1 && '; '}
+                                    </li>
+                                ))}
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
+            </span>
+        </div>
+    );
+}
+function displaySynonyms(taxon) {
+    if (taxon.dbXrefs && taxon.dbXrefs.filter(element => element.accession == null).length > 0) {
+        const filteredAndSortedElements = taxon.dbXrefs
+            .filter(element => element.accession == null)
+            .sort((a, b) => a.name.localeCompare(b.name));
+
+        const jsxElements = filteredAndSortedElements.map((element, index) => (
+            <span key={index}>
+                {element.name}
+                {index < filteredAndSortedElements.length - 1 && '; '}
+            </span>
+        ));
+
+        return (
+            <div className="key-value-pair">
+                <span className="key">Synonym(s)</span>
+                <span className="value">
+                    <ul className="xref">
+                        {jsxElements}
+                    </ul>
+                </span>
+            </div>
+        );
+    }
+
+    // Retourner null ou un autre élément React vide si nécessaire
+    return null;
 }
 
 const Species = () => {
@@ -61,9 +119,10 @@ const Species = () => {
         <main className={"container "}>
             <ChangePageTitle pageTitle={`${taxon ? taxon.scientificName : params.datasource + ":" + params.accession}`} />
             <div className="row">
-                <div className="col-md-12">
+                <div className="col-8 offset-2 text-center">
                     <h1>Taxon: {taxon ? taxon.scientificName : params.datasource + ":" + params.accession}</h1>
                 </div>
+                <div className="col-2 pt-2 text-end"><Link to="/species/search">New taxon search</Link></div>
             </div>
 
             { error && <div className={"container alert alert-danger"} role="alert">{error}</div> }
@@ -75,10 +134,9 @@ const Species = () => {
                             <span className="key">Scientific name</span> <span
                             className="value">{taxon.scientificName}</span>
                         </div>
-                        <div className="key-value-pair">
-                            <span className="key">Cross-reference(s)</span>
-                            <span className="value">{getXrefUl(taxon)}</span>
-                        </div>
+                        {displayXref(taxon)}
+                        {displaySynonyms(taxon)}
+                        
                         {lineage &&
                             <div className="key-value-pair">
                                 <span className="key">Lineage</span>
@@ -88,7 +146,7 @@ const Species = () => {
                                             <li key={index}>
                                                 {/*TODO replace <a> by <Link to={getMainUrl(element)} >{element.scientificName}</Link>*/}
                                                 <a href={getMainUrl(element)}>{element.scientificName}</a>
-                                                {index < lineage.length - 1 && <span className="separator"/>}
+                                                {index < lineage.length - 1 && <span className="lineage-separator"/>}
                                             </li>))}
                                     </ol>
                                 </span>
