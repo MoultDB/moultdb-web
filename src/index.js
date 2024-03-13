a7a78fea9a130eb4d1e6cf9fa529b72fb5d2cfb4
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2024-03-13 16:59:42 +0100

    Updade dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('moultdb-content')
-);
+const root = ReactDOM.createRoot(document.getElementById('moultdb-content'));
+root.render(<App />);
