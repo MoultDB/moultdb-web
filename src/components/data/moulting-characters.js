bc4b8b598d05e05d6dacfef7ac5728c3a6c113c2
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2024-09-25 11:47:21 +0200

    Update moulting characters columns and add 'customize columns' button

--- src/components/data/moulting-characters.js
+++ src/components/data/moulting-characters.js
@@ -15,65 +15,59 @@ $.DataTable = require( 'datatables.net-responsive-dt' )
 $.DataTable = require( 'datatables.net-searchbuilder-dt' );
 
 const columns = [
-    {
-        className: 'control',
-        orderable: false,
-        data: null,
-        render: function ( data, type, full ) {
-            return '';
-        },
-        targets:   -1
-    },
-    {   title: 'Taxon <div class="custom-tooltip">Finest-grained taxonomic level of organism (ideally species)</div>', 
-        data: 'taxon',
-        render: function(data, type, full) {
-            if (data) {
-                return '<a href=' + getSpeciesUrl(data) + '>' + data.scientificName + '</a>';
-            }
-            return '';
-        }
+    { title: 'Taxon', 
+      data: 'taxon',
+      render: function(data, type, full) {
+          if (data) {
+              let name = data.scientificName;
+              if (full.annotatedSpeciesName !== data.scientificName) {
+                  name = name + " (" + full.annotatedSpeciesName + ")";
+              }
+              return '<a href=' + getSpeciesUrl(data) + '>' + name + '</a>';
+          }
+          return '';
+      }
     },
     { title: 'Observed moult stage', data: 'condition.devStage.name' },
+    { title: 'Reproductive state', data: 'condition.reproductiveState' },
     { title: 'Type of specimens of interest', data: 'sampleSet.specimenTypes[, ]' },
     { title: 'Geological age', data: 'sampleSet.timePeriod',
-        render: function ( data, type, full ) {
-            if (data) {
-                if (data.geologicalAgeFrom.name === data.geologicalAgeTo.name) {
-                    return data.geologicalAgeFrom.name;
-                }
-                return data.geologicalAgeFrom.name + " - " + data.geologicalAgeTo.name;
-            }
-            return '';
-        }
+      render: function ( data, type, full ) {
+          if (data) {
+              if (data.geologicalAgeFrom.name === data.geologicalAgeTo.name) {
+                  return data.geologicalAgeFrom.name;
+              }
+              return data.geologicalAgeFrom.name + " to " + data.geologicalAgeTo.name;
+          }
+          return '';
+      }
     },
     { title: 'Location: name', data: 'sampleSet.collectionLocations[, ]' },
     { title : 'Contributor', data: 'version.creationUser',
-        render: function ( data, type, full ) {
-            if (data) {
-                return '<a href="https://orcid.org/' + data.orcidId + '">' + data.name + '</a>'
-            }
-            return '';
-        }
+      render: function ( data, type, full ) {
+          if (data) {
+              return '<a href="https://orcid.org/' + data.orcidId + '">' + data.name + '</a>'
+          }
+          return '';
+      }
     },
     { title: 'Moulting phase', data: 'moultingCharacters.moultingPhase' },
-    { title: 'Location of moulting suture', data: 'moultingCharacters.sutureLocation' },
-    { title: 'Direction of egress during moulting', data: 'moultingCharacters.egressDirection' },
-    { title: 'Position exuviae found in', data: 'moultingCharacters.positionExuviaeFoundIn' },
+    { title: 'Location of moulting suture', data: 'moultingCharacters.sutureLocations[, ]' },
+    { title: 'Direction of egress during moulting', data: 'moultingCharacters.egressDirections[, ]' },
+    { title: 'Position exuviae found in', data: 'moultingCharacters.positionsExuviaeFoundIn[, ]' },
     { title: 'Level of intraspecific variability in moulting mode', data: 'moultingCharacters.moultingVariability' },
-    { title: 'Other behaviours associated with moulting', data: 'moultingCharacters.otherBehaviour' },
+    { title: 'Other behaviours associated with moulting', data: 'moultingCharacters.otherBehaviours[, ]' },
     { title: 'Consumption of exuviae', data: 'moultingCharacters.exuviaeConsumption' },
     { title: 'Reabsorption during moulting', data: 'moultingCharacters.reabsorption' },
-    { title: 'Published reference: citation (APA style)', data: 'article',
-        render: function ( article, type, full ) {
-            if (article) {
-                let v = article.dbXrefs
-                    .map((element, index) => (
-                        `${element.dataSource.name}: <a href="${element.xrefURL}" rel="noopener noreferrer" target="_blank">${element.accession}</a>`
-                    ))
-                    .join(', ');
-                return `<div>${article.citation} ${v}</div>`;
-            }
-        }
+    { title: 'Published reference: citation', data: 'article',
+      render: function ( article, type, full ) {
+          if (article) {
+              let v = article.dbXrefs
+                  .map((element, index) => (`<a href="${element.xrefURL}" rel="noopener noreferrer" target="_blank">${element.accession}</a>`))
+                  .join(', ');
+              return `<div>${v}</div>`;
+          }
+      }
     },
     { title: 'Museum collection', data: 'sampleSet.storageLocations[, ]' },
     // { title: 'Museum accession', data: 'sampleSet.storageAccessions[, ]' },
@@ -82,7 +76,7 @@ const columns = [
     { title: 'Fossil preservation type', data: 'sampleSet.fossilPreservationTypes[, ]' },
     { title: 'Environment', data: 'sampleSet.environments[, ]' },
     { title: 'Life history style', data: 'moultingCharacters.lifeHistoryStyle' },
-    { title: 'Life mode', data: 'moultingCharacters.lifeMode' },
+    { title: 'Life mode', data: 'moultingCharacters.lifeModes[, ]' },
     { title: 'Number of juvenile moults', data: 'moultingCharacters.juvenileMoultCount' },
     { title: 'Number of major morphological transitions', data: 'moultingCharacters.majorMorphologicalTransitionCount' },
     { title: 'Adult stage moulting', data: 'moultingCharacters.hasTerminalAdultStage' },
@@ -93,22 +87,25 @@ const columns = [
     { title: '# body segments per moult stage', data: 'moultingCharacters.bodySegmentCount' },
     { title: 'Average body length increase from previous moult (in mm)', data: 'moultingCharacters.bodyLengthIncreaseAverage' },
     { title: 'Average body length (in mm)', data: 'moultingCharacters.bodyLengthAverage' },
+    { title: 'Average width length increase from previous moult (in mm)', data: 'moultingCharacters.bodyWidthIncreaseAverage' },
+    { title: 'Average width length (in mm)', data: 'moultingCharacters.bodyWidthAverage' },
     { title: 'Average body mass increase from previous moult (in g)', data: 'moultingCharacters.bodyMassIncreaseAverage' },
+    { title: 'Observed moult stage period (in days)', data: 'moultingCharacters.devStagePeriod' },
     { title: 'Intermoult period (in days)', data: 'moultingCharacters.intermoultPeriod' },
     { title: 'Pre-moult period (in days)', data: 'moultingCharacters.premoultPeriod' },
     { title: 'Post-moult period (in days)', data: 'moultingCharacters.postmoultPeriod' },
     { title: 'Variation in moulting time within cohorts (in days)', data: 'moultingCharacters.variationWithinCohorts' },
-    { title: 'Cephalic suture location', data: 'moultingCharacters.cephalicSutureLocation' },
-    { title: 'Post-cephalic suture location', data: 'moultingCharacters.postCephalicSutureLocation' },
-    { title: 'Resulting named moulting configurations', data: 'moultingCharacters.resultingNamedMoultingConfiguration' },
+    { title: 'Cephalic suture location', data: 'moultingCharacters.cephalicSutureLocations[, ]' },
+    { title: 'Post-cephalic suture location', data: 'moultingCharacters.postCephalicSutureLocations[, ]' },
+    { title: 'Resulting named moulting configurations', data: 'moultingCharacters.resultingNamedMoultingConfigurations[, ]' },
     { title: 'Post-moult cuticle calcification event', data: 'moultingCharacters.calcificationEvent' },
-    { title: 'Post-moult heavy metal reinforcement of the cuticle', data: 'moultingCharacters.heavyMetalReinforcement' },
-    { title: 'Quality of fossil exuviae', data: 'moultingCharacters.fossilExuviaeQuality' },
+    { title: 'Post-moult heavy metal reinforcement of the cuticle', data: 'moultingCharacters.heavyMetalReinforcements[, ]' },
+    { title: 'Quality of fossil exuviae', data: 'moultingCharacters.fossilExuviaeQualities[, ]' },
 ];
 
 class MoultingCharacters extends Component {
     componentDidMount() {
-        $(this.refs.mcharacters).DataTable({
+        const table = $(this.refs.mcharacters).DataTable({
             order: [[1, 'asc'], [2, 'asc']],
             scrollX: true,
             dom:"<'row'<'col'Q>>" +
@@ -127,16 +124,10 @@ class MoultingCharacters extends Component {
                 }
             },
             oLanguage: {
-                sSearch: "Global filter:"
+                sSearch: "Global filter"
             },
             lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]],
             columns,
-            responsive: {
-                details: {
-                    type: 'column',
-                    target: 'tr'
-                }
-            },
             ordering: true,
             data: this.props.mcData,
             buttons: [
@@ -153,7 +144,15 @@ class MoultingCharacters extends Component {
                 }
             ]
         });
+
+        // Allow to toggle the column visibility
+        $("input.toggle-vis").change(function() {
+            let columnIdx = this.getAttribute('data-column');
+            let column = table.column(columnIdx);
+            column.visible(this.checked);
+        });
     }
+    
     componentWillUnmount(){
         $('.data-table-wrapper').find('#mc-result').DataTable().destroy(true);
     }
@@ -161,14 +160,33 @@ class MoultingCharacters extends Component {
     render() {
         return (
             <div>
-                <div className={"result-info mb-3"}>
-                    <p>Clicking on the <span className={"open-row"}/> sign shows the full information for each row.</p>
+                <div className="dt-buttons">
+                    <button id="toggleColumnsButton" className="dt-button" type="button" data-bs-toggle="offcanvas"
+                            data-bs-target="#toggleColumns" aria-controls="toggleColumns">
+                        Customize columns
+                    </button>
                 </div>
 
+                <div className="offcanvas offcanvas-start" tabIndex="-1" id="toggleColumns"
+                     aria-labelledby="toggleColumnsLabel">
+                    <div className="offcanvas-header">
+                        <p className="offcanvas-title" id="toggleColumnsLabel">Customize columns</p>
+                        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div className="offcanvas-body">
+                        {columns.map((item, idx) => (
+                            <div key={"checkbox-div-" + idx}>
+                                <input id={"checkbox-" + idx} className="toggle-vis" data-column={idx}
+                                       type="checkbox" defaultChecked="true"/>
+                                <label htmlFor={"checkbox-" + idx}>{item.title}</label>
+                            </div>))}
+                    </div>
+                </div>
                 <table id="mc-result" ref="mcharacters"
-                       className={'table table-sm table-striped table-bordered table-hover responsive'}>
+                       className={'table table-sm table-striped table-bordered table-hover'}>
                 </table>
             </div>);
     }
 }
+
 export default MoultingCharacters;
