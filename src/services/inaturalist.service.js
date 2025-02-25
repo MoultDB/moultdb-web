d223672fb3e80ed9d5ae559b7c3a497964ba1d96
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2025-02-25 16:29:39 +0100

    Fix request to iNat API to match the one made by moulting.org

--- src/services/inaturalist.service.js
+++ src/services/inaturalist.service.js
@@ -5,6 +5,7 @@ class INaturalistService {
     getObservationsOfMoultingProject(iNatId, limit) {
         let url = "https://api.inaturalist.org/v1/observations?" +
             "project_id=moulting-arthropods&" +
+            "license=cc-by%2Ccc-by-nc%2Ccc0&" +
             "photo_license=cc-by%2Ccc-by-nc%2Ccc0&" +
             "per_page=" + (limit ? limit : 0) + "&" +
             "taxon_id=" + iNatId;
