cab5334e5bdd7aba4d7d665fdfb80da54dbf32e0
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2025-08-06 18:17:02 +0200

    Remove old code

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
-// import "./assets/bootstrap/css/bootstrap-grid.min.css";
-// import "./assets/bootstrap/css/bootstrap-reboot.min.css";
-// import "./assets/bootstrap/css/bootstrap.min.css";
-// FIXME should be kept?
 import "./assets/dropdown/css/style.css";
 import "./assets/socicon/css/styles.css";
 import "./assets/theme/css/style.css";
